ae9a4cd22697953c2c327720d1ad6b51258f7a78
Calli Crase <callilogancrase@gmail.com>
2019-08-12 14:12:34 -0600

    working app with no styling, add more default characters

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,6 +3,7 @@ var friendData = require("../data/friends");
 
 module.exports = function(app){
 
+    //returns all friends in the friends.js file as JSON
     app.get('/api/friends', function(req, res){
         res.json(friendData);
     });
@@ -11,9 +12,27 @@ module.exports = function(app){
         //used to handle incoming survey results and compatibility logic.
         var friend = req.body;
 
-        //parses scores to integers
+        //parse scores to integers
         for(var i = 0; i < friend.scores.length; i++){
             friend.scores[i] = parseInt(friend.scores[i]);
         };
+
+        var bffindex = 0;
+        var minDifference = 50;
+
+        for(var i = 0; i < friendData.length; i++){
+            var totalDiff = 0;
+            for(var j = 0; j < friendData[i].scores.length; j++){
+                var difference = Math.abs(friend.scores[j] - friendData[i].scores[j]);
+                totalDiff += difference;
+            };
+            if(totalDiff < minDifference){
+                bffindex = i;
+                minDifference = totalDiff;
+            };
+        };
+        friendData.push(friend);
+
+        res.json(friendData[bffindex]);
     });
 };
